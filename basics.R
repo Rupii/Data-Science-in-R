dacd76226d77128262a84b50fe6d6f31904dcf26
rupii <mudavathrupesh@gmail.com>
2018-02-09 19:06:26 +0530

    second commit

---
+++ basics.R
@@ -0,0 +1,34 @@
+# this is how you comment in R
+
+# Hello World!
+
+
+print("Hello World!")
+
+
+
+# Arthematic Operators
+3 + 9
+5 ^ 3
+# all the simpile calci operations can directly be performed by R on to console 
+
+
+# variable 
+name <- "Rupesh"
+# like any other scripting language R is a dynamically typed language
+# <- denotes assignment
+n = 103
+# = works too but <- is  general practice
+"Rupi" -> na # is valid too
+
+
+#dataTypes
+class(2.2)
+class(3)
+class('3f')
+class(T) # T and TRUE are both used used for logical # same for FALSE
+# all the foat and real numbers are of class numeric
+
+
+
+
