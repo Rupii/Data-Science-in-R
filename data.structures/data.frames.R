9e447dcabed99311aac8f8cb57483f1e29dbba1a
rupii <mudavathrupesh@gmail.com>
2018-02-14 22:42:59 +0530

    modified data frames

--- data.structures/data.frames.R
+++ data.structures/data.frames.R
@@ -9,6 +9,9 @@ id  <- c(105, 101, 102)
 #creating our own data frame
 df <- data.frame(names, id)
 
+# adding an extra coloumn
+df$extra <- c(3, 1, 2)
+
 #head gives top 6
 head(df)
 
@@ -26,3 +29,21 @@ df[, 1] #all rows first column
 
 df$names #similar to df["names"] but instead of dataframe we get vector
 
+
+# reading csv files
+
+df <- read.csv('file.csv') # file.csv(file names)
+
+df <- read.csv(file.choose()) #pops up the file manager to choose
+
+
+summary(df) # gives the statistical information on our dataframe
+
+#number of rows and coloumns
+
+nrow(df)
+ncol(df)
+
+
+# boolean masking
+df[df$id > 100]
